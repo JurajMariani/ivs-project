786dcd8a6fd374cacc0a3e85eb60bbc4a250d51a
JurajMariani <xmaria03@stud.fit.vutbr.cz>
2021-03-26 23:14:05 +0100

    Math library inclusion

---
+++ src/Header Files/math_lib.h
@@ -0,0 +1,25 @@
+//
+//
+//
+//
+//
+//
+//
+
+/**/
+
+double add (double x, double y);
+
+double sub (double x, double y);
+
+double mul (double x, double y);
+
+double div (double x, double y);
+
+double fact (double x);
+
+double power (double x, unsigned int n);
+
+double nqrt (double x, unsigned int n);
+
+double log (double x);
